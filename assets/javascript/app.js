ad5f49cf191846e6ed9cf5178d13aef2f65fb1a2
Carlos Ponce <jcponce510@gmail.com>
2019-03-01 23:25:56 -0800

    Add questions and radio buttons to index.html.

---
+++ assets/javascript/app.js
@@ -0,0 +1,8 @@
+$( document ).ready(function() {
+  $("a").click(function() {
+    // When start button is clicked, button disappears and timer & questions are displayed.
+    $(this).hide();
+    // Show timer & questions.
+
+  });
+});
