3b54aa8e43cc8a28a8ceba0d36d3bfa4d75afb3e
Carlos Ponce <jcponce510@gmail.com>
2019-04-30 16:29:56 -0700

    Add questions form.

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,8 +1,10 @@
 $( document ).ready(function() {
   $("a").click(function() {
+    
     // When start button is clicked, button disappears and timer & questions are displayed.
     $(this).hide();
+    
     // Show timer & questions.
-    $("#timAndQue").toggle();
+    $("form").toggle();
   });
 });
