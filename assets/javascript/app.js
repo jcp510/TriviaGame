07325b95d380946f96a01411c4175ffa8ed3fb21
Carlos Ponce <jcponce510@gmail.com>
2019-05-01 11:36:23 -0700

    Add timer countdown.

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,4 +1,6 @@
 $( document ).ready(function() {
+  var time = 30;
+
   $("a").click(function() {
     
     // When start button is clicked, button disappears and timer & questions are displayed.
@@ -6,5 +8,15 @@ $( document ).ready(function() {
     
     // Show timer & questions.
     $("form").toggle();
+
+    // Timer
+    $("#timer").text("Time Remaining: " + time + " seconds");
+
+    // Begin time countdown.
+    setTimeout(function run() {
+      time--;
+      $("#timer").text("Time Remaining: " + time + " seconds");
+      setTimeout(run, 1000);
+    }, 1000);
   });
 });
