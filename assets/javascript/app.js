87b025d5fef0e21a5b85894a8175933595b32782
Carlos Ponce <jcponce510@gmail.com>
2019-05-02 16:53:32 -0700

    Add logic for evaluating answers to questions.

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,5 +1,30 @@
+/*  ********* TODO *******************************
+*  Populate questions on page by jQuery rather than hard-coding html.
+*  Display missed questions/questions that had wrong answers when time expires.
+    ***********************************************  */
 $( document ).ready(function() {
-  var time = 30;
+  var time = 40;
+
+  // To track correct answers.
+  var correctAns = 0;
+
+  // To track wrong answers.
+  var wrongAns = 0;
+
+  // Logic for evaluating answers.
+  var evalAnswers = function() {
+    $(".key").each(function() {
+      if (this.checked) {
+        correctAns++;
+      } else {
+        wrongAns++;
+      }
+    });
+
+    // Display results.
+    $("#correct-answers").text("Correct Answers: " + correctAns);
+    $("#wrong-answers").text("Incorrect Answers: " + wrongAns);
+  };
 
   $("a").click(function() {
     
@@ -20,6 +45,13 @@ $( document ).ready(function() {
       // Stop timer when time runs out at zero seconds.
       if (time <= 0) {
         clearInterval(gogoGadgetTimer);
+        
+        // Hide timer and questions.
+        $("form").toggle();
+
+        // Display outcome of game.
+        $("#outcome").toggle();
+        evalAnswers();
       };
     }, 1000);
   });
